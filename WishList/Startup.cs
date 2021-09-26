6df3c514dad52404684848b2d4003dc3c4455f24
Rhutesh <69714305+Rhutesh@users.noreply.github.com>
2021-09-26 22:51:36 +0530

    step 3

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -15,16 +15,21 @@ namespace WishList
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services )
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async (context) =>
+            if (env.IsDevelopment() == true)
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseExceptionHandler("/Home/Error");
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); 
             });
         }
     }
